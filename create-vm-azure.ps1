02484d34b785f5afef27900579ebdf8411217df6
fazil-rf <88143808+fazil-rf@users.noreply.github.com>
2022-07-10 11:55:43 +0530

    Update and rename azuredeploy.json to create-vm-azure.ps1

---
+++ create-vm-azure.ps1
@@ -0,0 +1,30 @@
+# Login to Azure using Service Principal credentials from Github Secrets
+Write-Output "Logging in to Azure with a service principal..."
+az login `
+    --service-principal `
+    --username $Env:SP_CLIENT_ID `
+    --password $Env:SP_CLIENT_SECRET `
+    --tenant $Env:SP_TENANT_ID
+Write-Output "Done"
+
+# Select Azure subscription
+az account set `
+    --subscription $Env:AZURE_SUBSCRIPTION_NAME
+
+# Create the VM configuration object
+$ResourceGroupName = "RG-Infra-UpSkilling-July2022"
+$VmName = "fazil-Demo-vm"
+
+# Create a VM in Azure
+Write-Output "Creating VM..."
+try {
+    az vm create  `
+        --resource-group $ResourceGroupName `
+        --name $VmName `
+        --image win2016datacenter `
+        --admin-password $Env:SP_CLIENT_SECRET `
+    }
+catch {
+    Write-Output "VM already exists"
+    }
+Write-Output "Done creating VM"
